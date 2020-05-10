2ef52074de15198e96d732a23a3fe6519555032f
d.afonchenko <d.afonchenko@dodopizza.com>
2020-05-10 20:43:45 +0300

    Refactor tests by remove redundant assert

--- DDDCartAppTests/CartTests.cs
+++ DDDCartAppTests/CartTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using DDDCartAppDomain;
 using NUnit.Framework;
 
@@ -6,22 +7,33 @@ namespace DDDCartAppTests
 {
 	public class CartTests
 	{
+		private Cart _cart;
+
 		[SetUp]
 		public void Setup()
 		{
+			CartId cartId = new CartId($"cart-{Guid.NewGuid()}"); 
+			_cart = new Cart(cartId);
 		}
 
 		[Test]
-		public void WhenAddProductToCartEventReceived_ThenProductShouldBeAdded()
+		public void WhenAddProductToCartEventReceived_ThenCartShouldContainOneProduct()
 		{
-			CartId id = new CartId($"cart-{Guid.NewGuid()}"); 
-			Cart cart = new Cart(id);
 			AddProductEvent addProductEvent = new AddProductEvent();
 
-			cart.Apply(addProductEvent);
+			_cart.Apply(addProductEvent);
 
-			Assert.NotNull(cart.Products, "Products property should not be bull by default");
-			Assert.True(cart.Products.Count == 1);
+			Assert.NotNull(_cart.Products, "Products property should not be bull by default");
+			Assert.True(_cart.Products.Count == 1);
 		}
+
+		// [Test]
+		// public void WhenAddProductToCartEventReceived_ThenCartProductIdShouldBeEqualWithProductIdFromEvent()
+		// {
+			
+		// 	var cartProduct = cart.Products.First();
+		// 	Assert.NotNull(cartProduct.Id);
+		// 	Assert.True(cartProduct.Id == productId);
+		// }
 	}
 }
