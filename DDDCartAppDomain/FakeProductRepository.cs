6286989da4130ce78e07b9c05c7299589a992262
d.afonchenko <d.afonchenko@dodopizza.com>
2020-05-13 23:04:21 +0300

    Implement fake repository

---
+++ DDDCartAppDomain/FakeProductRepository.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DDDCartAppDomain
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        private readonly Dictionary<ProductId, Product> _repository;
+
+        public FakeProductRepository()
+        {
+            var productId = new ProductId(Guid.Empty);
+            
+            _repository = new Dictionary<ProductId, Product>()
+            {
+                {productId, new Product(productId, "Kolbasa", 200.0)}
+            };
+        }
+
+        public Task<Product> GetProduct(ProductId productId)
+        {
+            return Task.FromResult(_repository[productId]);
+        }
+    }
+}
